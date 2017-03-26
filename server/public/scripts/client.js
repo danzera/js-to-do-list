927243d20078b0ad8d1003a7ebff869ff1fcf95d
Dan Zera <dczera@gmail.com>
2017-03-26 15:37:18 -0500

    add a bit of table styling and additional logic to the DOM updates in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 function updateDOM(tasksArray) {
   var $tbody = $('#tasksTable').children().last();
-  //$tbody.empty();
+  $tbody.empty();
   for (var i = 0; i < tasksArray.length; i++) {
     var task = tasksArray[i];
     $tbody.append('<tr>');
@@ -13,7 +13,7 @@ function updateDOM(tasksArray) {
     $row.append('<td>' + task.name + '</td>');
     $row.append('<td>' + task.description + '</td>');
     $row.append('<td><input type="date" value=' + task.due_date.slice(0, 10) + '></input></td>');
-    $row.append('<td><input type="checkbox" name="complete" data-id="' + task.id + '"></td>');
+    $row.append('<td class="checkbox"><input type="checkbox" data-id="' + task.id + '"></td>');
     $row.append('<td><button class="delete" data-id="' + task.id + '">Delete Task</button></td>');
   }
 }
@@ -31,10 +31,9 @@ function getTasks() {
 
 // event listeners
 function addEventListeners() {
+  // 'Add Task!' click handler
   $('#addTask').on('submit', function(e) {
       e.preventDefault();
-      // TEST - WORKING
-      console.log('click listener working');
       var taskName = $('#taskName').val();
       var taskDescription = $('#taskDescription').val();
       var dueDate = $('#dueDate').val();
@@ -42,17 +41,25 @@ function addEventListeners() {
       if (taskName === '' || taskDescription === '' || dueDate === '') {
         alert('Please add input for all fields. Thank you, kindly.');
       } else {
+        clearInput();
         var task = {
           name: taskName,
           description: taskDescription,
           due: dueDate
         };
         // TEST - WORKING
-        console.log(task);
+        console.log('sending new task to the server:', task);
         addTask(task);
       } // end if-else
-  }); // end on-submit click listener
+  }); // end on-submit click listener for 'Add Task!' button
 } // end addEventListeners()
+
+// clear input fields
+function clearInput() {
+  $('#taskName').val('');
+  $('#taskDescription').val('');
+  $('#dueDate').val('');
+}
 
 // send new 'task' object to the server
 function addTask(task) {
@@ -62,7 +69,7 @@ function addTask(task) {
     data: task,
     success: function(res) {
       // TEST (after hooking up foam cup on the server) - WORKING
-      console.log(res);
+      getTasks();
     }
   });
 } // end newTask()
